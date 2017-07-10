84c5a19b92dfdb1873213ea4956dba54b7959b08
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-10 09:28:41 +0700

    Thêm SV BLL

--- Source code/SV/BLL.cs
+++ Source code/SV/BLL.cs
@@ -7,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace QLKTX.SV
 {
-    class BLL
+    public class BLL
     {
-       
+        DAL dal = new DAL();
+        public DTO[] GetList()
+        {
+            return dal.GetSV();
+        }
+
+ //Thêm xóa sửa sinh viên
+        public void themsinhvien(string MSV, string MKTX, string H, string T, string CMND, bool GT, DateTime NS, string SDT, string QQ, DateTime NLHD, string Mphong, string Hinh, string HTGH, string SDTGH, string QH, string NN)
+        {
+            dal.themsv(MSV,MKTX,H,T,CMND,GT,NS,SDT,QQ,NLHD,Mphong,Hinh,HTGH,SDTGH,QH,NN);
+        }
+
     }
 }
