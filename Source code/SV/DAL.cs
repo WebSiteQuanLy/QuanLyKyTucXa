ecd17927a88728610c26ec89d631da6954457ece
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-10 09:04:34 +0700

    khởi tạo knoi
    khởi tạo knoi với database

--- Source code/SV/DAL.cs
+++ Source code/SV/DAL.cs
@@ -8,8 +8,17 @@ using System.Data;
 
 namespace QLKTX.SV
 {
-    class DAL
-    {  
-
-    }
+     public class DAL
+    {
+        DataHelper helper = new DataHelper();//khởi tạo một kết nói mới từ database
+   
+        private  DTO ParseData(DataRow row)
+        {
+            DTO SV = new DTO();
+            SV.MaSV = row["MaSV"].ToString().Trim();
+            SV.MaKTX = row["MaKTX"].ToString().Trim();
+            SV.Ho = row["Ho"].ToString().Trim();
+            SV.Ten = row["Ten"].ToString().Trim();
+        }
+     }
 }
