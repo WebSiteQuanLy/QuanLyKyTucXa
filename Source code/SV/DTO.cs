56285da69e67cd13b97c2d7eaefe287efcb681cf
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-10 08:22:32 +0700

    DTO SV
    vận chuyển dữ liệu giữa các tầng

---
+++ Source code/SV/DTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKTX.SV
+{
+    public class DTO
+    {
+        public string MaSV { get; set; }
+        public string MaKTX { get; set; }
+        public string Ho { get; set; }
+        public string Ten { get; set; }
+        public string CMND { get; set; }
+    }
+}
