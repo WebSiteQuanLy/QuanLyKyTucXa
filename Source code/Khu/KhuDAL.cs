0d7a65b753371e892df25900de236e79c57873f8
mylinh123 <mylinhqn2002@gmail.com>
2017-07-11 22:46:48 +0700

    Lớp DAL của bảng khu

---
+++ Source code/Khu/KhuDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QLKTX.Khu
+{
+    public class KhuDAL
+    {
+        DataHelper helper = new DataHelper();
+
+        private KhuDTO ParseData(DataRow row)
+        {
+            KhuDTO khu = new KhuDTO();
+            khu.MaKhu = row["MaKhu"].ToString().Trim();
+            khu.Tenkhu = row["TenKhu"].ToString().Trim();
+            return khu;
+        }
