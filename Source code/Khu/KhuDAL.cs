b2eda67b5a2696f0bb13b2d4cf3d53037daff34a
mylinh123 <mylinhqn2002@gmail.com>
2017-07-11 22:49:10 +0700

    Chú thích lớp DAL

--- Source code/Khu/KhuDAL.cs
+++ Source code/Khu/KhuDAL.cs
@@ -9,12 +9,12 @@ namespace QLKTX.Khu
 {
     public class KhuDAL
     {
-        DataHelper helper = new DataHelper();
+        DataHelper helper = new DataHelper();/// tạo mới một helper có chức năng vận chuyển như DâtHelper
 
         private KhuDTO ParseData(DataRow row)
         {
             KhuDTO khu = new KhuDTO();
-            khu.MaKhu = row["MaKhu"].ToString().Trim();
-            khu.Tenkhu = row["TenKhu"].ToString().Trim();
+            khu.MaKhu = row["MaKhu"].ToString().Trim();///Lấy kiểu dữ liệu Mã khu
+            khu.Tenkhu = row["TenKhu"].ToString().Trim();///Lấy kiểu dữ liệu tên khu
             return khu;
         }
