c32d896a0f5ca3e055fb988f2c0b24b065dd3458
mylinh123 <mylinhqn2002@gmail.com>
2017-07-11 22:38:08 +0700

    Lớp vận chuyển dữ liệu DTO cho dữ liệu ở bảng khu

---
+++ Source code/Khu/KhuDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKTX.Khu
+{
+    public class KhuDTO
+    {
+        public string MaKhu {get;set;}
+        public string Tenkhu{get;set;}
+     
+    }
+}
