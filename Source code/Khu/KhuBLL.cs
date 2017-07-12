3bdd1a2d8b5bcf16330abedb76c7f130255e9cb0
mylinh123 <mylinhqn2002@gmail.com>
2017-07-12 21:22:11 +0700

    Class BLL

---
+++ Source code/Khu/KhuBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKTX.Khu
+{
+    public class KhuBLL
+    {
+        KhuDAL dal = new KhuDAL();
+        public KhuDTO[] GetList()
+        {
+            return dal.GetKhu();
+        }
+    }
+}
