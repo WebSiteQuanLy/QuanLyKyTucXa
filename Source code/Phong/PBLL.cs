916bdacb868749bb602c6e761bd511c0972a01c6
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-13 19:54:59 +0700

    code thêm xóa sửa phòng

--- Source code/Phong/PBLL.cs
+++ Source code/Phong/PBLL.cs
@@ -16,5 +16,18 @@ namespace QLKTX.Phong
             return Ph.Getphong();
         }
         
+         public void themph(string MP,string MK,string TP,bool LP,int SNHT,int SNTD)
+        {
+            Ph.themphong(MP,MK,TP,LP,SNHT,SNTD);
+        }
+        public void xoaph(string mp)
+        {
+            Ph.xoaph(mp);
+        }
+        public void suaph(string Mp, string Mk, string Tp, bool Lp, int Snht, int Sntd)
+        {
+            Ph.suaph(Mp, Mk, Tp, Lp, Snht, Sntd);
+        }
+
     }
 }
