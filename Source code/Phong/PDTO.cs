969bb2a47cd3c6fd8facdfc87f0cdcc3c0712780
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-13 19:15:10 +0700

    DTO phòng
    file code DTO phòng

---
+++ Source code/Phong/PDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKTX.Phong
+{
+    public class PDTO
+    {
+        public string Maphong { get; set; }
+        public string Makhu { get; set; }
+        public string Tenphong { get; set; }
+        public bool Loaiphong { get; set; }
+        public int Songuoihientai { get; set; }
+        public int Songuoitoida { get; set; }
+        
+    }
+}
