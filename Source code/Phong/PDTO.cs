a0e12f15e041fcc1e36a2334995e8c1359ae3bac
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-13 19:18:54 +0700

    thêm chú thích file DTO phong

--- Source code/Phong/PDTO.cs
+++ Source code/Phong/PDTO.cs
@@ -8,12 +8,12 @@ namespace QLKTX.Phong
 {
     public class PDTO
     {
-        public string Maphong { get; set; }
-        public string Makhu { get; set; }
-        public string Tenphong { get; set; }
-        public bool Loaiphong { get; set; }
-        public int Songuoihientai { get; set; }
-        public int Songuoitoida { get; set; }
+        public string Maphong { get; set; }//dữ liệu mã phòng trong bảng dữ liệu phòng
+        public string Makhu { get; set; }//dữ liệu mã khu trong bảng dữ liệu khu
+        public string Tenphong { get; set; }//lấy dữ liệu tên phòng trong bảng dữ liệu
+        public bool Loaiphong { get; set; }//lấy dữ liệu loại phòng trong bảng dữ liệu
+        public int Songuoihientai { get; set; } //lấy dữ liệu số người hiện tại trong bảng dữ liệu
+        public int Songuoitoida { get; set; } //lấy dữ liệu số người tối đa trong bảng dữ liệu
         
     }
 }
