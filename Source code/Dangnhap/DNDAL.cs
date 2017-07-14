1511333962eaca22d01b9fd1eb1a0f3adca4f8ca
mylinh123 <mylinhqn2002@gmail.com>
2017-07-14 11:13:25 +0700

    thêm
    Thêm chú thích file DAL Đăng nhập

--- Source code/Dangnhap/DNDAL.cs
+++ Source code/Dangnhap/DNDAL.cs
@@ -9,17 +9,17 @@ namespace QLKTX.Dangnhap
 {
     pubilc class DNDAL
     {
-       DataHelper helper = new DataHelper();
+       DataHelper helper = new DataHelper();///tạo mới một data helper
 
         private DNDTO ParseData(DataRow row)
         {
             DNDTO DN = new DNDTO();
-            DN.username = row["username"].ToString().Trim();
-            DN.Pass = row["Pass"].ToString().Trim();
-            DN.Hovaten = row["Hovaten"].ToString().Trim();
-            DN.Gioitinh = (bool)row["Gioitinh"];
-            DN.Quyen = row["Quyen"].ToString().Trim();
-            DN.SDT = row["SDT"].ToString().Trim();
+            DN.username = row["username"].ToString().Trim();///Lấy kiểu dữ liệu user name
+            DN.Pass = row["Pass"].ToString().Trim();//Lấy kiểu dữ liệu mật khẩu
+            DN.Hovaten = row["Hovaten"].ToString().Trim();//Lấy kiểu dữ liệu Hoj và tên
+            DN.Gioitinh = (bool)row["Gioitinh"];//Lấy kiểu dữ liệu trường giới tính
+            DN.Quyen = row["Quyen"].ToString().Trim();//Lấy kiểu dữ liệu quyền
+            DN.SDT = row["SDT"].ToString().Trim();//Lấy kiểu dữ liệu Số điện thoại
             return DN;
         }
 
