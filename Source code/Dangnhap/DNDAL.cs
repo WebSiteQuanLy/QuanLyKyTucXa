f2fabb296921ece75514001ee74ace4f1a12f466
mylinh123 <mylinhqn2002@gmail.com>
2017-07-14 11:09:01 +0700

    sửa file DAL đăng nhập

--- Source code/Dangnhap/DNDAL.cs
+++ Source code/Dangnhap/DNDAL.cs
@@ -7,8 +7,21 @@ using System.Data;
 
 namespace QLKTX.Dangnhap
 {
-    class DNDAL
+    pubilc class DNDAL
     {
-        
+       DataHelper helper = new DataHelper();
+
+        private DNDTO ParseData(DataRow row)
+        {
+            DNDTO DN = new DNDTO();
+            DN.username = row["username"].ToString().Trim();
+            DN.Pass = row["Pass"].ToString().Trim();
+            DN.Hovaten = row["Hovaten"].ToString().Trim();
+            DN.Gioitinh = (bool)row["Gioitinh"];
+            DN.Quyen = row["Quyen"].ToString().Trim();
+            DN.SDT = row["SDT"].ToString().Trim();
+            return DN;
+        }
+ 
     }
 }
