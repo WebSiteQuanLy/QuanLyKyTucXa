74fd63471e4328a972a9766e0ba2075e41bbaa51
mylinh123 <mylinhqn2002@gmail.com>
2017-07-14 11:07:06 +0700

    thêm chú thích file DTO

--- Source code/Dangnhap/DNDTO.cs
+++ Source code/Dangnhap/DNDTO.cs
@@ -8,11 +8,11 @@ namespace QLKTX.Dangnhap
 {
     public class DNDTO
     {
-        public string username { get; set; }
-        public string Pass { get; set; }
-        public string Hovaten { get; set; }
-        public bool Gioitinh { get; set; }
-        public string Quyen { get; set; }
-        public string SDT { get; set; }
+        public string username { get; set; }///lấy dữ liệu tên user name trong bảng dữ liệu dangnhap
+        public string Pass { get; set; }///lấy dữ liệu mật khẩu trong bảng dữ liệu dangnhap
+        public string Hovaten { get; set; }///lấy dữ liệu họ và tên người đăng nhập trong bảng dữ liệu dangnhap
+        public bool Gioitinh { get; set; }///lấy dữ liệu giới tính trong bảng dữ liệu dangnhap
+        public string Quyen { get; set; }///lấy dữ liệu quyền trong bảng dữ liệu dangnhap
+        public string SDT { get; set; }///lấy dữ liệu SDT trong bảng dữ liệu dangnhap
     }
 }
