53101fbdb131474b9b10998684bd2aeed282fa7c
mylinh123 <mylinhqn2002@gmail.com>
2017-07-14 11:04:33 +0700

    file DTo đăng nhập

---
+++ Source code/Dangnhap/DNDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKTX.Dangnhap
+{
+    public class DNDTO
+    {
+        public string username { get; set; }
+        public string Pass { get; set; }
+        public string Hovaten { get; set; }
+        public bool Gioitinh { get; set; }
+        public string Quyen { get; set; }
+        public string SDT { get; set; }
+    }
+}
