5175351d46b7316cf76cc38a5aa1e7d4c158ff22
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-15 12:56:23 +0700

    sửa file DAL điện nước

--- Source code/Diennuoc/DiennuocDAL.cs
+++ Source code/Diennuoc/DiennuocDAL.cs
@@ -8,8 +8,20 @@ using QLKTX.Diennuoc;
 
 namespace QLKTX.Diennuoc
 {
-   class DiennuocDAL
+   public class DiennuocDAL
     {
-       
+       DataHelper helper = new DataHelper();//tạo mới một helper
+        private DiennuocDTO ParseData(DataRow row)
+        {
+            DiennuocDTO DN = new DiennuocDTO();
+            DN.Maphong = row["Maphong"].ToString().Trim();//Lấy kiểu dữ liệu Mã phòng
+            DN.Thang = (DateTime)row["Thang"];//Lấy kiểu dữ liệu tháng
+            DN.CScudien = (int)row["CScudien"];//Lấy kiểu dữ liệu cs điện cũ
+            DN.CSmoidien = (int)row["CSmoidien"];//Lấy kiểu dữ liệu cs điện mới
+            DN.CScunuoc = (int)row["CScunuoc"];//Lấy kiểu dữ liệu cs nước cũ
+            DN.CSmoinuoc = (int)row["CSmoinuoc"];//Lấy kiểu dữ liệu cs nước mới
+          
+            return DN;
+        }
     }
 }
