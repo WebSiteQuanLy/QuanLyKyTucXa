d2d3beada6c7c6e7f82488d32a6f8fbb3642cb40
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-15 13:05:46 +0700

    file điện nước BLL

---
+++ Source code/Diennuoc/DiennuocBLL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKTX.Diennuoc
+{
+    public class DiennuocBLL
+    {
+        DiennuocDAL DN = new DiennuocDAL();
+        public DiennuocDTO[] GetlistDN()
+        {
+            return DN.GetDN();
+        }
+
+
+        public void luudn(string MP, DateTime TH, int CSDC, int CSDM, int CSNC, int CSNM)
+        {
+            DN.luudiennuoc(MP, TH, CSDC, CSDM, CSNC, CSNM);
+        }
+        public void suadn(string MP, DateTime TH, int CSDC, int CSDM, int CSNC, int CSNM)
+        {
+            DN.suadiennuoc(MP, TH, CSDC, CSDM, CSNC, CSNM);
+        }
+        public void xoadn(string MP)
+        {
+            DN.xoadiennuoc(MP);
+        }
+
+    }
+}
