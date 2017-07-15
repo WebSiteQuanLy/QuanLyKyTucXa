2064703e7ae94ddb376405c735822abf3e49696f
MauBao <ngomaubaodhcn1b@gmail.com>
2017-07-15 12:53:50 +0700

    file DAL điện nước

---
+++ Source code/Diennuoc/DiennuocDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QLKTX.Diennuoc;
+
+namespace QLKTX.Diennuoc
+{
+   class DiennuocDAL
+    {
+       
+    }
+}
